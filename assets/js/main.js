521683aef77550da055df0119585f40076c55b0c
Kharun <harun.mashanlo12@gmail.com>
2025-07-08 22:23:27 +0600

    added all pages

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+const overlay = $(".overlay");
+
+$(document).ready(function () {
+  $(".offset_gallery_item").on("click", function () {
+    overlay.addClass("active");
+    const $this = $(this);
+
+    let imagesHtml = "";
+    $this.find(".offset_gallery_images img").each(function () {
+      const src = $(this).attr("src");
+      imagesHtml += `
+        <div class="swiper-slide">
+          <img src="${src}" />
+        </div>
+      `;
+    });
+
+    $(".offset_gallery_modal .offset_gallery_modal_swiper_2 .swiper-wrapper").html(imagesHtml);
+    $(".offset_gallery_modal .offset_gallery_modal_swiper .swiper-wrapper").html(imagesHtml);
+
+    $(".offset_gallery_modal").addClass("active");
+
+    if (typeof galleryThumbs2 !== "undefined") galleryThumbs2.update();
+    if (typeof galleryThumbs !== "undefined") galleryThumbs.update();
+  });
+});
+
+$(".offset_gallery_close").click(function () {
+  overlay.removeClass("active");
+  $(".offset_gallery_modal").removeClass("active");
+});
